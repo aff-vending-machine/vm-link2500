11f435c50cafc2a88186ac98176a7e586996a9dc
Tanawat Hongthai <ztrixack.th@gmail.com>
2023-05-26 09:41:02 +0700

    🐛 fix(main.go): fix import paths to match the new project structure ✨ feat(dockerfile): add support for setting the Asia/Bangkok timezone and copy the vendor folder if it exists to speed up build time. Also, remove unnecessary root user permissions and add device flag to CMD to allow access to ttyACM0.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/aff-vending-machine/vm-link2500/config"
-	"github.com/aff-vending-machine/vm-link2500/internal/boot/registry/app"
-	"github.com/aff-vending-machine/vm-link2500/pkg/boot"
-	"github.com/aff-vending-machine/vm-link2500/pkg/log"
+	"vm-link2500/config"
+	"vm-link2500/internal/boot/registry/app"
+	"vm-link2500/pkg/boot"
+	"vm-link2500/pkg/log"
 )
 
 func init() {
@@ -18,23 +18,12 @@ func main() {
 	defer boot.Serve()
 
 	initLog(conf)
-	// initTrace(conf)
 
 	// Run main application
 	app.Run(conf)
 }
 
 func initLog(conf config.BootConfig) {
-	if conf.App.ENV == "local" {
-		log.SetOutput(log.ColorConsole())
-	}
+	log.SetOutput(log.ColorConsole())
 	log.SetLogLevel(conf.App.LogLevel)
 }
-
-// func initTrace(conf config.BootConfig) {
-// 	endpoint := "http://localhost:14268/api/traces"
-// 	provider, err := trace.Jaeger(endpoint, "raspi-ctrl", conf.App.ENV)
-// 	boot.TerminateWhenError(err)
-
-// 	trace.SetProvider(provider)
-// }
